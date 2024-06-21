83204208240705ad8240e33e2f94405d336b29bb
Ravi Kumar <ravikr12082018@gmail.com>
2024-06-21 23:51:20 +0530

    Theme and Routing is done

---
+++ SCM_UI/src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Service from './components/Service/Service'
+import { ThemeProvider } from './contexts/Theme'
+import { useState, useEffect } from 'react'
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+     <Route path='/' element={<Layout/>}>
+       <Route path='' element={<Home/>}></Route>
+       <Route path='about' element={<About/>}></Route>
+       <Route path='contact' element={<Contact/>}></Route>
+       <Route path='service' element={<Service/>}></Route>
+     </Route>
+    )
+ )
+
+function App() {
+
+    const [themeMode, setThemeMode] = useState(localStorage.getItem("theme") === null ? "light" : localStorage.getItem("theme"));
+
+    const lightTheme = () => {
+        setThemeMode("light");
+    }
+
+    const darkTheme = () => {
+        setThemeMode("dark");
+    }
+
+    useEffect(() => {
+        document.querySelector('html').classList.remove("light", "dark");
+        document.querySelector('html').classList.add(themeMode);
+        localStorage.setItem("theme",themeMode)
+    }, [themeMode])
+
+    return (
+        <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+            <RouterProvider router={router} />
+        </ThemeProvider>
+    )
+}
+
+export default App
---
+++ scm/src/main/java/com/scm/scm/models/User.java
@@ -0,0 +1,8 @@
+package com.scm.scm.models;
+
+public class User {
+    public String name;
+    public User(String name){
+        this.name = name;
+    }
+}
